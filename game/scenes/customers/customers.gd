2716e21a0f80e60bff31cdecf45c0d7f3e2daacd
Ján Chudý <chudyjan23@gmail.com>
2023-11-27 21:56:54 +0100

    Modified and added more customers

--- game/scenes/customers/customers.gd
+++ game/scenes/customers/customers.gd
@@ -7,16 +7,22 @@ class_name CustomerManager
 @onready var dialogue_player: DialoguePlayer = %DialoguePlayer
 
 var customer_bodies: Array[Texture2D] = [
-	preload("res://sprites/concept/customer/0.png"),
-	preload("res://sprites/concept/customer/1.png"),
-	preload("res://sprites/concept/customer/2.png"),
-	preload("res://sprites/concept/customer/3.png")
+	preload("res://sprites/customers/0/0.png"),
+	preload("res://sprites/customers/1/1.png"),
+	preload("res://sprites/customers/2/2.png"),
+	preload("res://sprites/customers/3/3.png"),
+	preload("res://sprites/customers/4/4.png"),
+	preload("res://sprites/customers/5/5.png"),
+	preload("res://sprites/customers/6/6.png")
 ]
 var customer_heads: Array[Texture2D] = [
-	preload("res://sprites/concept/customer/0_head.png"),
-	preload("res://sprites/concept/customer/1_head.png"),
-	preload("res://sprites/concept/customer/2_head.png"),
-	preload("res://sprites/concept/customer/3_head.png")
+	preload("res://sprites/customers/0/0_head.png"),
+	preload("res://sprites/customers/1/1_head.png"),
+	preload("res://sprites/customers/2/2_head.png"),
+	preload("res://sprites/customers/3/3_head.png"),
+	preload("res://sprites/customers/4/4_head.png"),
+	preload("res://sprites/customers/5/5_head.png"),
+	preload("res://sprites/customers/6/6_head.png")
 ]
 
 func _ready():
